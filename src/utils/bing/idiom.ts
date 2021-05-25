4260c434b6b73906a47d35261dd66fc30bc38197
潘少军 <1760406005@qq.com>
2021-05-25 10:38:16 +0800

    add 解析常用成语

---
+++ src/utils/bing/idiom.ts
@@ -0,0 +1,45 @@
+type Idiom={
+    key:string,
+    list:string[]
+}
+export type Idioms=Idiom[]
+
+export const getIdioms=(dom: HTMLDocument)=>{
+    const ans:Idioms=[];
+    const idiomDom=dom.querySelector('.idomseg');
+    if(idiomDom){
+        const childs=idiomDom.children;
+        for(let i=0,len=childs.length/2;i<len;i++){
+            const keyDom=childs[i*2].querySelector('.defitemtitle');
+            const valDom=childs[i*2+1].querySelector('.defitemcon');
+            const examItem=childs[i*2+1].querySelectorAll('.examitem');
+            ans.push(getIdiom(keyDom,valDom,examItem));
+        }
+    }
+    return ans;
+}
+
+/**
+ * 
+ * @param keyDom 关键词
+ * @param valDom 例子
+ * @param examItem 额外的例子
+ */
+const getIdiom=(keyDom:Element|null,valDom:Element|null,examItem:NodeListOf<Element>)=>{
+    const ans:Idiom={
+        key:'',
+        list:[]
+    }
+    if(keyDom){
+        ans.key=keyDom.innerHTML;
+    }
+    if(valDom){
+        ans.list.push(valDom.innerHTML);
+    }
+    if(examItem.length){
+        for(let j of examItem){
+            ans.list.push(j.innerHTML);
+        }
+    }
+    return ans;
+}
