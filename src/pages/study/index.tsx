756b1aed2494a816fec88c6b21be6b4b1a5870ec
潘少军 <1760406005@qq.com>
2021-05-25 18:10:00 +0800

    test 测试

--- src/pages/study/index.tsx
+++ src/pages/study/index.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react';
-
+import { getDom } from '@/utils/bing';
+import { getAuido } from '@/utils/bing/auido';
+import { getDefinition } from '@/utils/bing/definition';
+import { getEnglishChinese } from '@/utils/bing/english-chinese-dictionary';
+import { getExamples } from '@/utils/bing/example';
+import { getIdioms } from '@/utils/bing/idiom';
+import { getKey } from '@/utils/bing/key-word';
+import { getSider } from '@/utils/bing/sider';
+import { getWordChange } from '@/utils/bing/word-change';
 const Study = () => {
-    const [like, setLike] = useState(1);
-    const handleClick = () => {
-        setTimeout(() => {
-            alert(`you clicked ${like} times`)
-        }, 1000)
+    const handleClick = async () => {
+        const dom = await getDom("word");
+        if (dom) {
+            console.log("声音", getAuido(dom));
+            console.log("定义", getDefinition(dom));
+            console.log("英汉", getEnglishChinese(dom));
+            console.log("例子", getExamples(dom));
+            console.log("成语", getIdioms(dom));
+            console.log("关键词", getKey(dom));
+            console.log("侧边栏", getSider(dom));
+            console.log("词性转换", getWordChange(dom));
+        }
     }
     return (
         <div>
-            <button onClick={() => setLike(like + 1)}>你点击了{like}次</button>
-            <button onClick={handleClick}>显示点击次数</button>
+            <button onClick={handleClick}>测试</button>
         </div>
     )
 }
