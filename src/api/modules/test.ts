03b1f3f6aff4ad1e835a199d7f0a4ccc79af6850
潘少军 <1760406005@qq.com>
2021-05-26 10:08:54 +0800

    mod 文件api

--- src/api/modules/test.ts
+++ src/api/modules/test.ts
@@ -1,12 +1,26 @@
-import {api} from '@/api';
+import { api } from '@/api';
+import { to } from 'await-to-js';
 
-export const fetchFile=(fileStr:string)=>{
-    if(fileStr===''){
-        return;
+export const fetchFile = async (fileStr: string) => {
+    if (fileStr === '') {
+        return false;
     }
-    const fileUrl=fileStr;
-    const fileName=fileStr.split('/').slice(5).join('');
-    if(fileUrl&&fileName){
-        api.post("fetchfile",{fileUrl,fileName})
+    const fileUrl = fileStr;
+    const fileName = fileStr.split('/').slice(5).join('');
+    if (fileUrl && fileName) {
+        const [e, data] = await to(api.post('test/fetchfile', { fileName, fileUrl }));
+        if (e) {
+            console.log(e);
+            return false;
+        } else {
+            const { code } = data?.data;
+            if (code === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }else{
+        return false;
     }
 }
